e30c4c6e8d363cfdf7b95de0d7dd2c6e093001cf
ChrisTimmers <christimmers2000@gmail.com>
2024-05-25 01:55:48 +0200

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -59,7 +59,7 @@ def assign_players():
                 for i in range(120):
                     random_player = random.choice(possible_players2)
                     display_assignment(placeholder, position, random_player, 0)
-                    time.sleep(0.00025*(0.5*i)**2)
+                    time.sleep(0.0002*(0.5*i)**2)
                 display_assignment(placeholder, position, selected_player, 1)
                 time.sleep(3)
             display_assignment(placeholder, position, selected_player, 1)
